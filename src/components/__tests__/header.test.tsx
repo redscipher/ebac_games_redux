356f9149fa53a3665760a63cfe70770311f50e33
reds <redlemagic@hotmail.com>
2024-10-30 22:25:28 -0300

    subindo testes

--- src/components/__tests__/header.test.tsx
+++ src/components/__tests__/header.test.tsx
@@ -1,17 +1,41 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import Header from '../Header'
-import { Provider } from 'react-redux'
-import { store } from '../../store'
+import { renderizarComProvider } from '../../utils/tests'
 
 describe('Teste p/ comp. Header', () => {
   test('Renderizar corretamente', () => {
-    const { debug } = render(
-      <Provider store={store}>
-        <Header></Header>
-      </Provider>
-    )
-
-    debug()
+    renderizarComProvider(<Header></Header>)
     expect(screen.getByText('EBAC Games')).toBeInTheDocument()
+  })
+
+  test('Deve renderizar com 2 itens no carrinho', () => {
+    renderizarComProvider(<Header></Header>, {
+      preloadedState: {
+        carrinho: {
+          itens: [
+            {
+              id: 1,
+              categoria: 'Categ1',
+              imagem: 'Imagem1',
+              plataformas: ['PS5', 'Xbox Series X'],
+              preco: 100,
+              precoAntigo: 120,
+              titulo: 'Titulo1'
+            },
+            {
+              id: 2,
+              categoria: 'Categ2',
+              imagem: 'Imagem2',
+              plataformas: ['PS5', 'Xbox Series X'],
+              preco: 90,
+              precoAntigo: 120,
+              titulo: 'Titulo2'
+            }
+          ]
+        }
+      }
+    })
+
+    expect(screen.getByTestId('id-qtd-carrinho').innerHTML).toContain('2')
   })
 })
